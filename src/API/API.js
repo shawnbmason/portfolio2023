42bc817f0c6a0846125428b5dd51fb2506558c28
shawnbmason <s.mason1993@yahoo.com>
2023-12-06 09:38:04 -0800

    contact form varification

---
+++ src/API/API.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+//  ******* Get IP adress *******
+export const IpAddress = async ({
+  setLoading,
+  setIpData,
+  //   setIpDataCity,
+  //   setIpDataRegion,
+}) => {
+  try {
+    let res = await axios.get(
+      // 👇 check out (.env) folder to see where I got this information
+      `http://api.ipstack.com/check?access_key=${process.env.REACT_APP_IP_ADDRESS_API_KEY}`
+    );
+    if (res) {
+      setLoading(false);
+      setIpData(res.data.country_name);
+      //   setIpDataCity(res.data.city);
+    }
+  } catch (error) {
+    alert("IP address Error: &{error}");
+  }
+};
---
+++ src/Componets/loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div style={{ color: "cornflowerblue" }}>
+      <i className="fa fa-dot-circle animate-ping">Loading...</i>
+    </div>
+  );
+}
